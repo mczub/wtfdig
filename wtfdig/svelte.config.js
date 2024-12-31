665800357fd847898dd4e3e08bd96a12aac9ff77
matthew czubakowski <mczub@mczub.com>
2024-12-31 00:44:06 -0800

    fix config?

--- wtfdig/svelte.config.js
+++ wtfdig/svelte.config.js
@@ -1,12 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { importAssets } from 'svelte-preprocess-import-assets' 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess()],
+	preprocess: [vitePreprocess(), importAssets()],
 	
 	kit: {
 		adapter: adapter({
