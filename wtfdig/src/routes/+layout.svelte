3ed2079cb2b88352332b562eb7e245964d6150d7
matthew czubakowski <mczub@mczub.com>
2025-01-02 01:45:49 -0800

    made footer only sticky on big screens

--- wtfdig/src/routes/+layout.svelte
+++ wtfdig/src/routes/+layout.svelte
@@ -40,7 +40,7 @@
 
 <slot />
 
-<footer class="sticky bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+<footer class="md:sticky bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
     <div class="flex w-full max-w-screen-xl mx-auto justify-between">
 		<div class="text-sm text-gray-500 dark:text-gray-400">
             <div>Comments? Questions? Something incorrect? <a target="_blank" rel="noopener noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline" href="https://docs.google.com/forms/d/e/1FAIpQLScJEJ43FKjSRJ2MyLuGXznce-P_SQNyPLWga_Xme_CJKPiQIQ/viewform?usp=header">Let me know.</a></div>
