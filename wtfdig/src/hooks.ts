c06c5aaff319edea099d787f9d6befa50df8d7c5
matthew czubakowski <mczub@mczub.com>
2025-03-22 22:52:38 -0700

    fix prerender

--- wtfdig/src/hooks.ts
+++
@@ -1,8 +0,0 @@
-import type { Reroute } from '@sveltejs/kit';
-
-export const reroute: Reroute = ({ url }) => {
-    console.log(url.pathname)
-	if (url.pathname === '/') {
-		return '/71/chaotic';
-	}
-};
