2be3ceb9904659825c2eb267354fe366e015bc46
matthew czubakowski <mczub@mczub.com>
2025-03-27 23:25:36 -0700

    add ex4

--- wtfdig/src/routes/+layout.svelte
+++ wtfdig/src/routes/+layout.svelte
@@ -93,6 +93,12 @@
                 {#snippet content()}
                 <article>
                     <div class="opacity-60 preset-outlined-surface-200-800 grid grid-rows-4 p-0 w-full divide-y divide-surface-800 rounded-md bg-surface-950">
+                        <div class="px-4 py-2">
+                            <a href="{base}/72/ex4" class="flex flex-col w-full items-start">
+                                <div class="text-lg -mb-2">Recollection</div>
+                                <div class="text-sm">Extreme</div>
+                            </a>
+                        </div>
                         <div class="px-4 py-2 cursor-not-allowed">
                             <div class="flex flex-col w-full items-start cursor-not-allowed">
                                 <div class="text-lg -mb-2">AAC Cruiserweight M1</div>
