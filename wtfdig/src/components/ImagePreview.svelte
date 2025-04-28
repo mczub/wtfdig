c9f2a1ef295fe9d146aa66f902651a911fd29bb4
matthew czubakowski <mczub@mczub.com>
2025-04-27 22:34:11 -0700

    add a border to image preview

--- wtfdig/src/components/ImagePreview.svelte
+++ wtfdig/src/components/ImagePreview.svelte
@@ -30,7 +30,7 @@
 <Modal
   open={imageOpenState}
   onOpenChange={(e) => (imageOpenState = e.open)}
-  contentBase="card bg-surface-100-900 p-4 space-y-4 shadow-xl flex flex-col"
+  contentBase="card bg-surface-100-900 p-4 space-y-4 shadow-xl flex flex-col border border-surface-600"
   backdropClasses="backdrop-blur-sm"
   zIndex={"3000"}
 >
