fbea1766cfade14020c7a86d9b965a57d754d7c6
matthew czubakowski <mczub@mczub.com>
2026-05-02 17:27:51 -0700

    upgrades

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  plugins: [tailwindcss(), sveltekit()] as any,
+  plugins: [tailwindcss(), sveltekit()],
   test: {
     include: ['tests/**/*.test.ts']
   }
